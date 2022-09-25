949764c737d0ce6b4f43713a3a05392dc5966436
hao759 <qhao74154@gmail.com>
2022-09-25 21:50:21 +0700

    update

--- src/services/chatbotService.js
+++ src/services/chatbotService.js
@@ -5,7 +5,7 @@ import request from "request";
 const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
-let callSendAPI = (sender_psid,response) => {
+let callSendAPI = (sender_psid, response) => {
   let request_body = {
     recipient: {
       id: sender_psid,
@@ -31,19 +31,47 @@ let callSendAPI = (sender_psid,response) => {
 };
 
 let handleGetStarted = (sender_psid) => {
-  return new Promise(async(resole, reject) => {
+  return new Promise(async (resole, reject) => {
     try {
-        console.log("vo nek");
-        let response = { text: "Hello :), started rồi đó." };
-        await callSendAPI(sender_psid,response);
-        resole('done');
+      let userName = await getUserName(sender_psid);
+      let response = { text: `Hello ${userName} :), started rồi đó.` };
+      await callSendAPI(sender_psid, response);
+      resole("done");
     } catch (error) {
-        reject(error);
+      reject(error);
     }
+  });
+};
+
+let getUserName = (sender_psid) => {
+  // let userName= "";
+  // let request_body = {
+  //     recipient: {
+  //       id: sender_psid,
+  //     },
+  //     message: response,
+  //   };
+  // Send the HTTP request to the Messenger Platform
+  return new Promise((resole, reject) => {
+    request(
+      {
+        uri: `https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic&access_token=${PAGE_ACCESS_TOKEN}`,
+        method: "get",
+      },
+      (err, res, body) => {
+        if (!err) {
+          let body = JSON.parse(body);
+          userName = `${body.first_name} ${body.last_name}`;
+          resole(userName);
+        } else {
+          console.error("Unable to send message:" + err);
+        }
+      }
+    );
   });
 };
 
 module.exports = {
   handleGetStarted,
-  callSendAPI
+  callSendAPI,
 };
