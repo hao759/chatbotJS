f858449e52233e814c0b476a856923e47883015d
hao759 <qhao74154@gmail.com>
2022-09-28 12:00:51 +0700

    update

--- src/public/js/reserve_table.js
+++ src/public/js/reserve_table.js
@@ -25,8 +25,8 @@ console.log("------------------------------------")
 };
 
 //validate inputs
-function validateInputFields() {
-    console.log("----------------------")
+// function validateInputFields() {
+    // console.log("----------------------")
 //   const EMAIL_REG = /[a-zA-Z][a-zA-Z0-9_\.]{1,32}@[a-z0-9]{2,}(\.[a-z0-9]{2,4}){1,2}/g;
 
 //   let email = $("#email");
@@ -46,15 +46,52 @@ function validateInputFields() {
 //       phoneNumber.removeClass("is-invalid");
 //   }
 
-  return true;// false;
-}
+//   return true;// false;
+// }
 
 
-MessengerExtensions.requestCloseBrowser(function success() {
-    // webview closed
-  }, function error(err) {
-    // an error occurred
-  });
+function handleClickButtonReserveTable() {
+    $("#btnFindOrder").on("click", function (e) {
+      let check = true;
+      // check=; //return true or false
+      console.log("----------------------");
+      let data = {
+        psid: $("#psid").val(),
+        customerName: $("#customerName").val(),
+        email: $("#email").val(),
+        phoneNumber: $("#phoneNumber").val(),
+      };
+
+      if (!check) {
+        //close webview
+        MessengerExtensions.requestCloseBrowser(
+          function success() {
+            console.log("--------------------");
+            // webview closed
+          },
+          function error(err) {
+            // an error occurred
+            console.log(err);
+          }
+        );
+
+        //send data to node.js server
+        $.ajax({
+          url: `${window.location.origin}/reserve-table-ajax`,
+          method: "POST",
+          data: data,
+          success: function (data) {
+            console.log(data);
+          },
+          error: function (error) {
+            console.log(error);
+          },
+        });
+      }
+    });
+  }
+
+
 
 // function handleClickButtonReserveTable() {
 //   $("#btnFindOrder").on("click", function (e) {
